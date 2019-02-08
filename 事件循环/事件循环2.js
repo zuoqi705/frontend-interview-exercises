b490f73cc8101083dffd158904d14ef17eb554c4
mavericker-1996 <1398772969@qq.com>
2019-02-08 21:22:01 +0800

    添加一些图片

--- 事件循环/事件循环2.js
+++ 事件循环/事件循环2.js
@@ -32,7 +32,9 @@ new Promise(function(resolve) {
 console.log('script end');
 
 
-/*变式
+
+
+/*变式1
 script start
 async1 start
 promise1
@@ -71,3 +73,52 @@ new Promise(function(resolve) {
 });
 
 console.log('script end');
+
+
+
+
+/*变式2
+script start
+a1 start
+a2
+promise2
+script end
+promise1
+promise2.then
+promise3
+a1 end
+setTimeout
+*/
+async function a1 () {
+    console.log('a1 start')
+    await a2()
+    console.log('a1 end')
+}
+async function a2 () {
+    console.log('a2')
+}
+
+console.log('script start')
+
+setTimeout(() => {
+    console.log('setTimeout')
+}, 0)
+
+Promise.resolve().then(() => {
+    console.log('promise1')
+})
+
+a1()
+
+let promise2 = new Promise((resolve) => {
+    resolve('promise2.then')
+    console.log('promise2')
+})
+
+promise2.then((res) => {
+    console.log(res)
+    Promise.resolve().then(() => {
+        console.log('promise3')
+    })
+})
+console.log('script end')
