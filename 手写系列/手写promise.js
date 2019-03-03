ffefe51f38f5c7ea12d7ef3e85a3aa7df7f51ff3
佐七 <zuoqi.zzq@alibaba-inc.com>
2019-03-03 17:57:23 +0800

    添加手写Promise

--- 手写系列/手写promise.js
+++ 手写系列/手写promise.js
@@ -30,4 +30,38 @@ function MyPromise(fn) {
   } catch (e) {
     reject(e);
   }
+
+  MyPromise.prototype.then = function(onFulfilled, onRejected) {
+    const that = this;
+    //防止透传报错
+    onFulfilled = typeof onFulfilled === "function" ? onFulfilled : v => v;
+    onRejected =
+      typeof onRejected === "function"
+        ? onRejected
+        : r => {
+            throw r;
+          };
+    if (that.state === PENDING) {
+      that.resolvedCallbacks.push(onFulfilled)
+      that.rejectedCallbacks.push(onRejected)
+    }
+
+    if (that.state === RESOLVED) {
+      onFulfilled(that.value)
+    }
+
+    if(that.state === REJECTED) {
+      onRejected(that.value)
+    }
+  };
 }
+
+
+new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1)
+  }, 0)
+  console.log('2')
+}).then(value => {
+  console.log(value)
+})
